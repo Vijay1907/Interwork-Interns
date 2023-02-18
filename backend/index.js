bd715bfc8384672d2f440c0281622e3a1714865d
Ajay Raj <ajayraj072001@gmail.com>
2023-02-18 21:37:30 +0530

    added my apis

--- backend/index.js
+++ backend/index.js
@@ -75,6 +75,66 @@ app.put('/updateuser', async(req,res)=>{
   })
 
 
+  // 3. main
+  app.post('/adduserdata', async(req,res)=>{
+    let {userName,userEmail, userContact, businessContact, businessName, password, country, city, address} = req.body;
+    let db =mongoutil.getDb();
+    if(db){
+      if(Object.keys(req.body).length>8){
+      let data=await db.collection(collection).findOne({userEmail:userEmail},{projection:{_id:0}})
+      if(data){
+        res.status(200).send({message: "email already exist. Try another one",success: false})
+      }else{
+          data = await db.collection(collection).insertOne({
+          userEmail:userEmail,
+          userName: userName,
+          userContact: userContact,
+          businessContact: businessContact,
+          businessName: businessName,
+          password: password,
+          country: country,
+          city: city,
+          address: address})
+          console.log(data)
+          if(data.acknowledged){
+            res.status(200).send({message: "user added successfully",success: true})
+          }else{
+            res.status(404).send({message : "Oops, Some error occured" ,success: false});
+          }
+      }
+    }else{
+      res.status(404).send({message : "Oops, Some error occured" ,success: false});
+    }
+     
+  }else{
+      res.status(404).send({message:"databse is not connected",success: false})
+  }
+})
+
+// 4. main
+app.post('/credentials',async(req,res)=>{
+  let db = mongoutil.getDb()
+  if(db){
+   const {userEmail,password}= req.body;
+   if(userEmail,password){
+    let data=await db.collection(collection).findOne({userEmail:userEmail},{projection:{_id:0}})
+    if(data){
+     if(data.password===password){
+      res.status(200).send({message:"logged in successfully",success: true})
+     }else{
+      res.status(200).send({message:"wrong password",success: false})
+     }
+    }else{
+      res.status(200).send({message:"Please signup first",success: false})
+    }
+   }else{
+    res.status(404).send({message:"databse is not connected",success: false})
+  }
+  }else{
+    res.status(404).send({message:"databse is not connected",success: false})
+  }
+})
+
  
 // listening port 
  app.listen(port,  ()=>{
