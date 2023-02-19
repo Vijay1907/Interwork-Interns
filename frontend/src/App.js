7997cc16c0de5f3d5ca77aa7427c46339b3dea6e
yogeshSingh8 <yogeshsingh1729@gmail.com>
2023-02-19 21:26:53 +0530

    Add Login File

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import Login from './components/Login';
+import UsersDetail from './components/UsersDetail';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {useState} from 'react'
+
+function App() {
+
+  const[email,setEmail]= useState("")
+   
+  const findLength=(obj)=>{
+    let count=0;
+     for(let keys in obj){
+        if(obj[keys].length>0){
+         count++;
+        }
+     }
+     return count;
+ }
+
+
+  return (
+    <>
+     <BrowserRouter>
+       <Routes>
+           <Route exact path="/login" element={<Login findLength={findLength} setEmail={setEmail}/>} />
+           <Route exact path="/userDetail" element={<UsersDetail  email={email}/>} />
+       </Routes>
+     </BrowserRouter>
+      </> 
+       
+  )
+}
+
+export default App;
